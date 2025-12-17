fcf2f53b8bc583f7df07c944d3aa6cf58806fe6c
gluu <pfan3065@gmail.com>
2025-12-17 16:01:21 +0800

    feat: update code

--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
-import ollama from 'ollama'
+// import ollama from 'ollama'
 
-const response = await ollama.chat({
-  model: "deepseek-r1",
-  messages: [{ role: 'user', content: '为什么天空是蓝色的?' }],
-})
+import { ChatOllama } from "@langchain/ollama";
 
-console.log(response.message.content)
+// const response = await ollama.chat({
+//   model: "deepseek-r1",
+//   messages: [{ role: 'user', content: '为什么天空是蓝色的?' }],
+// })
+
+// console.log(response.message.content)
+
+
+const model = new ChatOllama({
+  model: "qwen3-vl:8b", // Default value.
+  baseUrl:"http://127.0.0.1:11434",
+  temperature: 0.7, 
+   numPredict: 1000,
+   topP: 0.7,
+   frequencyPenalty: 0,
+   presencePenalty: 0,
+});
+
+const messages = [
+  { role: "system", content: "You are a poetry expert" },
+  { role: "user", content: "Write a haiku about spring" },
+  { role: "assistant", content: "Cherry blossoms bloom..." },
+];
+// const result = await model.invoke(["human", "Hello, how are you?"]);
+
+const result = await model.invoke(messages);
+
+console.log(result);
